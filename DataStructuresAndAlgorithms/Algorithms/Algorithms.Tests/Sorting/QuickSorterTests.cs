7dd9dced7a42b35d83ff49c95fa0413a0afa1a07
Tim Johnson <timjohnsonpdx0@gmail.com>
2021-10-24 19:54:06 -0700

    updating tests file

--- DataStructuresAndAlgorithms/Algorithms/Algorithms.Tests/Sorting/QuickSorterTests.cs
+++ DataStructuresAndAlgorithms/Algorithms/Algorithms.Tests/Sorting/QuickSorterTests.cs
@@ -18,7 +18,6 @@ namespace Algorithms.Tests.Sorting
             var values = new int[] { 1, 10, 2, 9, 3, 8, 4, 7, 5, 6 };
             var expected = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-            // Apply quicksort asdfafda
             QuickSorter.Sort(values);
 
             for (int i = 0; i < values.Length; i++)
