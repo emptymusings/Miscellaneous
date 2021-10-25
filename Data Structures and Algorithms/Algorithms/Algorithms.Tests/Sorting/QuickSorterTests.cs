e530a86607f7d58873db9150831d2ea15db0685b
Tim Johnson <timjohnsonpdx0@gmail.com>
2021-10-24 17:45:59 -0700

    Initial commit of QuickSort

---
+++ Data Structures and Algorithms/Algorithms/Algorithms.Tests/Sorting/QuickSorterTests.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithms.Tests.Sorting
+{
+    using Algorithms.Sorting;
+
+    [TestClass]
+    public class QuickSorterTests
+    {
+        Random _random;
+
+        [TestMethod]
+        public void SetIntegers_Ascending_Succeeds()
+        {
+            var values = new int[] { 1, 10, 2, 9, 3, 8, 4, 7, 5, 6 };
+            var expected = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            QuickSorter.Sort(values);
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                var expectedValue = expected[i];
+                var actual = values[i];
+                Assert.AreEqual(expectedValue, actual);
+            }
+        }
+
+        [TestMethod]
+        public void RandomIntegers_Ascending_SmallPositive_Succeeds()
+        {
+            _random = new Random(0);
+            List<int> values = new List<int>();
+
+            for (int i = 0; i < 10000; i++)
+            {
+                values.Add(_random.Next(-5000, int.MaxValue));
+            }
+
+
+            var actual = values.ToArray();
+            values.Sort();
+            QuickSorter.Sort(actual);
+            int idx = 0;
+            foreach(int item in values)
+            {
+                Assert.AreEqual(item, actual[idx]);
+                idx++;
+            }
+        }
+
+        [TestMethod]
+        public void RandomChars_Ascending_Succeeds()
+        {
+            List<char> actual = new List<char>();
+            List<char> expected = new List<char>();
+
+            _random = new Random(0);
+
+            for (int i = 0; i <= 26; i++)
+            {
+                var letterValue = _random.Next(97, 122);
+                actual.Add((char)letterValue);
+                expected.Add((char)letterValue);
+            }
+
+            expected.Sort();
+            var sorted = actual.ToArray();
+
+            QuickSorter.Sort(sorted);
+
+            int idx = 0;
+
+            foreach(var c in sorted)
+            {
+                Assert.AreEqual(expected[idx], c);
+                idx++;
+            }
+
+        }
+    }
+}
---
+++ Data Structures and Algorithms/Algorithms/Algorithms/Sorting/QuickSorter.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Algorithms.Sorting
+{
+    public class QuickSorter
+    {
+        public static void  Sort<T>(T[] values) where T : IComparable
+        {
+            PerformSort(values, 0, values.Length - 1);
+        }
+
+        private static void PerformSort<T>(T[] values, int left, int right) where T : IComparable
+        {
+            int pivot = Partition(values, left, right);
+
+            if (left < right)
+            {
+                if (pivot > 1)
+                {
+                    PerformSort(values, left, pivot - 1);
+                }
+
+                if (pivot + 1 < right)
+                {
+                    PerformSort(values, pivot, right);
+                }
+            }
+        }
+
+        private static int Partition<T>(T[] values, int left, int right) where T : IComparable
+        {
+            T pivot = values[left];
+
+            while(true)
+            {
+                while (values[left].CompareTo(pivot) < 0)
+                {
+                    left++;
+
+                    if (left == values.Length)
+                    {
+                        return right;
+                    }
+                }
+
+                while (values[right].CompareTo(pivot) > 0)
+                {
+                    right--;
+                }
+
+                if (left < right)
+                {
+                    if (values[left].CompareTo(values[right]) == 0)
+                    {
+                        right--;
+                    }
+
+                    if (values[left].CompareTo(values[right]) >= 0)
+                    {
+                        T temp = values[left];
+                        values[left] = values[right];
+                        values[right] = temp;
+                    }
+                }
+                else
+                {
+                    return right + 1;
+                }
+            }
+        }
+    }
+}
