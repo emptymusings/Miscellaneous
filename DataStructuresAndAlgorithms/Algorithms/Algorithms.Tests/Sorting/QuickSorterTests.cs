3147a06fa226b37cf57a32745451022a4eaf3b69
Tim Johnson <timjohnsonpdx0@gmail.com>
2021-10-25 21:35:23 -0700

    Added descending sort

--- DataStructuresAndAlgorithms/Algorithms/Algorithms.Tests/Sorting/QuickSorterTests.cs
+++ DataStructuresAndAlgorithms/Algorithms/Algorithms.Tests/Sorting/QuickSorterTests.cs
@@ -30,6 +30,23 @@ namespace Algorithms.Tests.Sorting
         }
 
         [TestMethod]
+        public void SetIntegers_Descending_Succeeds()
+        {
+            var values = new int[] { 1, 10, 2, 9, 3, 8, 4, 7, 5, 6 };
+            var expected = new int[] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+
+            // Apply quicksort asdfafda
+            QuickSorter.Sort(values, SortDirections.Descending);
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                var expectedValue = expected[i];
+                var actual = values[i];
+                Assert.AreEqual(expectedValue, actual);
+            }
+        }
+
+        [TestMethod]
         public void RandomIntegers_Ascending_SmallPositive_Succeeds()
         {
             _random = new Random(0);
@@ -46,6 +63,31 @@ namespace Algorithms.Tests.Sorting
             QuickSorter.Sort(actual);
             int idx = 0;
             foreach(int item in values)
+            {
+                Assert.AreEqual(item, actual[idx]);
+                idx++;
+            }
+        }
+
+        [TestMethod]
+        public void RandomIntegers_Descending_SmallPositive_Succeeds()
+        {
+            _random = new Random(0);
+            List<int> values = new List<int>();
+
+            for (int i = 0; i < 10000; i++)
+            {
+                values.Add(_random.Next(-5000, int.MaxValue));
+            }
+
+            var actual = values.ToArray();
+            var ordered = values.OrderByDescending(x => x);
+            values = ordered.ToList();
+
+            QuickSorter.Sort(actual, SortDirections.Descending);
+
+            int idx = 0;
+            foreach (int item in values)
             {
                 Assert.AreEqual(item, actual[idx]);
                 idx++;
@@ -81,5 +123,38 @@ namespace Algorithms.Tests.Sorting
             }
 
         }
+
+        [TestMethod]
+        public void RandomChars_Descending_Succeeds()
+        {
+            List<char> actual = new List<char>();
+            List<char> expected = new List<char>();
+
+            _random = new Random(0);
+
+            for (int i = 0; i <= 26; i++)
+            {
+                var letterValue = _random.Next(97, 122);
+                actual.Add((char)letterValue);
+                expected.Add((char)letterValue);
+            }
+
+            var ordered = expected.OrderByDescending(x => x);
+            expected = ordered.ToList();
+
+            var sorted = actual.ToArray();
+
+            QuickSorter.Sort(sorted, SortDirections.Descending);
+
+            int idx = 0;
+
+            foreach (var c in sorted)
+            {
+                Assert.AreEqual(expected[idx], c);
+                idx++;
+            }
+
+        }
+
     }
 }
--- DataStructuresAndAlgorithms/Algorithms/Algorithms/Sorting/QuickSorter.cs
+++ DataStructuresAndAlgorithms/Algorithms/Algorithms/Sorting/QuickSorter.cs
@@ -2,38 +2,46 @@
 {
     using System;
 
+    public enum SortDirections
+    {
+        Ascending,
+        Descending
+    }
+
     public class QuickSorter
     {
-        public static void  Sort<T>(T[] values) where T : IComparable
+        public static void  Sort<T>(T[] values, SortDirections sortDirection = SortDirections.Ascending) where T : IComparable
         {
-            PerformSort(values, 0, values.Length - 1);
+            PerformSort(values, 0, values.Length - 1, sortDirection);
         }
 
-        private static void PerformSort<T>(T[] values, int left, int right) where T : IComparable
+        private static void PerformSort<T>(T[] values, int left, int right, SortDirections sortDirection) where T : IComparable
         {
-            int pivot = Partition(values, left, right);
+            int pivot = Partition(values, left, right, sortDirection);
 
             if (left < right)
             {
                 if (pivot > 1)
                 {
-                    PerformSort(values, left, pivot - 1);
+                    PerformSort(values, left, pivot - 1, sortDirection);
                 }
 
                 if (pivot + 1 < right)
                 {
-                    PerformSort(values, pivot, right);
+                    PerformSort(values, pivot, right, sortDirection);
                 }
             }
         }
 
-        private static int Partition<T>(T[] values, int left, int right) where T : IComparable
+        private static int Partition<T>(T[] values, int left, int right, SortDirections sortDirection) where T : IComparable
         {
             T pivot = values[left];
-
+            
             while(true)
             {
-                while (values[left].CompareTo(pivot) < 0)
+                while (
+                    (sortDirection == SortDirections.Ascending && values[left].CompareTo(pivot) < 0) || 
+                    (sortDirection == SortDirections.Descending && values[left].CompareTo(pivot) > 0))
                 {
                     left++;
 
@@ -43,7 +51,10 @@
                     }
                 }
 
-                while (values[right].CompareTo(pivot) > 0)
+                while (
+                    (sortDirection == SortDirections.Ascending && values[right].CompareTo(pivot) > 0) || 
+                    (sortDirection == SortDirections.Descending && values[right].CompareTo(pivot) < 0)
+                    )
                 {
                     right--;
                 }
@@ -55,7 +66,8 @@
                         right--;
                     }
 
-                    if (values[left].CompareTo(values[right]) >= 0)
+                    if ((sortDirection == SortDirections.Ascending && values[left].CompareTo(values[right]) >= 0) || 
+                        sortDirection == SortDirections.Descending && values[left].CompareTo(values[right]) <= 0)
                     {
                         T temp = values[left];
                         values[left] = values[right];
