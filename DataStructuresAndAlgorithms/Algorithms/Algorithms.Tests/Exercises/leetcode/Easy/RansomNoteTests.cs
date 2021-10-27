a25e8e48da1bee760376588e03092b83b8ea0507
Tim Johnson <timjohnsonpdx0@gmail.com>
2021-10-26 19:15:52 -0700

    Added RansomNote exercise

---
+++ DataStructuresAndAlgorithms/Algorithms/Algorithms.Tests/Exercises/leetcode/Easy/RansomNoteTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithms.Tests.Exercises.leetcode.Easy
+{
+    using Algorithms.Exercises.leetcode.Easy;
+
+    [TestClass]
+    public class RansomNoteTests
+    {
+        [TestMethod]
+        public void LeetcodeTests_Succeeds()
+        {
+            string[] values = new string[]
+            {
+                "a",
+                "b",
+                "aa",
+                "ab",
+                "aa",
+                "aab",
+            };
+            
+            Assert.IsFalse(RansomNote.CanConstruct("a", "b"));
+            Assert.IsFalse(RansomNote.CanConstruct("aa", "ab"));
+            Assert.IsTrue(RansomNote.CanConstruct("aa", "aab"));
+        }
+    }
+}
---
+++ DataStructuresAndAlgorithms/Algorithms/Algorithms/Exercises/leetcode/Easy/RansomNote.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms.Exercises.leetcode.Easy
+{
+    public class RansomNote
+    {
+        public static bool CanConstruct(string ransomNote, string magazine)
+        {
+            if (string.IsNullOrEmpty(ransomNote)) return true;
+            if (string.IsNullOrEmpty(magazine) || ransomNote.Length > magazine.Length) return false;
+
+            if (ransomNote.Length == 1)
+            {
+                return magazine.IndexOf(ransomNote) >= 0;
+            }
+
+            var ransomeLookup = test(ransomNote);
+            var magazineLookup = test(magazine);
+
+            foreach (KeyValuePair<char, int> item in ransomeLookup)
+            {
+                if (!magazineLookup.ContainsKey(item.Key)) return false;
+                if (magazineLookup[item.Key] < item.Value) return false;
+            }
+
+            return true;
+        }
+
+        private static Dictionary<char, int> test(string source)
+        {
+            Dictionary<char, int> result = new Dictionary<char, int>();
+            int length = (int)source.Length / 2;
+
+            for (int i = 0; i < length; i++)
+            {
+                if (!result.ContainsKey(source[i]))
+                {
+                    result.Add(source[i], 1);
+                }
+                else
+                {
+                    result[source[i]] += 1;
+                }
+
+                if (!result.ContainsKey(source[source.Length - 1 - i]))
+                {
+                    result.Add(source[source.Length - 1 - i], 1);
+                }
+                else
+                {
+                    result[source[source.Length - i - 1]] += 1;
+                }
+            }
+
+            if (length * 2 < source.Length)
+            {
+                if (result.ContainsKey(source[length]))
+                {
+                    result[source[length]] += 1;
+                }
+                else
+                {
+                    result.Add(source[length], 1);
+                }
+            }
+
+            return result;
+        }
+    }
+}
