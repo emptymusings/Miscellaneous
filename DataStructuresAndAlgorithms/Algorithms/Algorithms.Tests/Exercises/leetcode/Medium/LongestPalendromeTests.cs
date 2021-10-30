c9d453a53a725116c4498e6a00d992396bb9fa54
Tim Johnson <51803216+lamentary@users.noreply.github.com>
2021-10-29 20:12:19 -0700

    Added test for brute force palendrome (#13)

--- DataStructuresAndAlgorithms/Algorithms/Algorithms.Tests/Exercises/leetcode/Medium/LongestPalendromeTests.cs
+++ DataStructuresAndAlgorithms/Algorithms/Algorithms.Tests/Exercises/leetcode/Medium/LongestPalendromeTests.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Algorithms.Tests.Exercises.leetcode.Medium
 {
-    class LongestPalendromeTests
+    using Algorithms.Exercises.leetcode.Medium;
+
+    [TestClass]
+    public class LongestPalendromeTests
     {
+        [TestMethod]
+        public void LeetcodeTests_Succeeds()
+        {
+            var test = new LongestPalendromeBruteForce();
+
+            var results = test.LongestPalindrome("babad");
+            Assert.AreEqual("bab", results);
+
+            results = test.LongestPalindrome("cbbd");
+            Assert.AreEqual("bb", results);
+
+            results = test.LongestPalindrome("a");
+            Assert.AreEqual("a", results);
+
+            results = test.LongestPalindrome("ac");
+            Assert.AreEqual("a", results);
+        }
     }
 }
