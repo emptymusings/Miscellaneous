f9a075a05b6edfdb8bb4a96d3be4493de9dc4a79
Tim Johnson <51803216+lamentary@users.noreply.github.com>
2021-11-13 11:37:10 -0800

    Brute force test (#14)
    * Added test for brute force palendrome
    
    * Added React app base setup
    
    * Added more algorithms and tests

---
+++ DataStructuresAndAlgorithms/Algorithms/Algorithms.Tests/Exercises/HackerRank/HackerRankTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Algorithms.Exercises.HackerRank;
+
+namespace Algorithms.Tests.Exercises.HackerRank
+{
+    [TestClass]
+    public class HackerRankTests
+    {
+        [TestMethod]
+        public void SampleOneTest()
+        {
+            List<List<string>> list = new List<List<string>>();
+
+            list.Add(new List<string>() { "p1", "1", "2" });
+            list.Add(new List<string>() { "p2", "2", "1" });
+            //list.Add(new List<string>() { "item3", "17", "8" });
+
+            List<string> expected = new List<string>() { "p1" };
+            List<string> actual = WebPagination.fetchItemsToDisplay(list, 0, 0, 1, 0);
+
+            Assert.AreEqual(expected.Count(), actual.Count());
+
+            for (int i = 0; i < expected.Count(); i++)
+            {
+                Assert.AreEqual(expected[i], actual[i]);
+            }
+        }
+    }
+}
---
+++ DataStructuresAndAlgorithms/Algorithms/Algorithms/Exercises/leetcode/Medium/StringToInteger.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms.Exercises.leetcode.Medium
+{
+    public class StringToInteger
+    {
+        public int MyAtoi(string s)
+        {
+            bool inDigits = false;
+            bool isNegative = false;
+            int result = 0;
+
+            var test = 4 % 5;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == ' ' && !inDigits)
+                {
+                    continue;
+                }
+
+                if ((s[i] == '-' || s[i] == '+') && !inDigits)
+                {
+                    isNegative = s[i] == '-';
+
+                    if (i + 1 < s.Length && char.IsNumber(s[i + 1]))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        return 0;
+                    }
+                }
+
+                if (!char.IsNumber(s[i]))
+                {
+                    break;
+                }
+
+                if (char.IsNumber(s[i]))
+                {
+                    inDigits = true;
+
+                    if (result == 0)
+                    {
+                        result = Int32.Parse(s[i].ToString());
+                    }
+                    else
+                    {
+
+                        try
+                        {
+                            checked
+                            {
+                                result = (result * 10) + int.Parse(s[i].ToString());
+                            }
+                        }
+                        catch(OverflowException)
+                        {
+                            return isNegative ? Int32.MinValue : Int32.MaxValue;
+                        }
+                    }
+                }
+            }
+
+            return isNegative ? result * -1 : result;
+        }
+    }
+}
