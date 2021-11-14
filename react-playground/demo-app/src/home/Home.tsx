928fc2337c3984f20cdfbaf0b270189426dcc5f9
Tim Johnson <timjohnsonpdx0@gmail.com>
2021-11-14 15:47:06 -0800

    Added type lint from functions+custom prettier

--- react-playground/demo-app/src/home/Home.tsx
+++ react-playground/demo-app/src/home/Home.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Paper } from "@mui/material";
+import React from 'react';
+import { Paper } from '@mui/material';
 
 export default function Home(): JSX.Element {
   return (
