3f9bd3a551c2c17765d1a5d452dd8806f9f0eb7e
Tim Johnson <timjohnsonpdx0@gmail.com>
2021-11-21 13:32:21 -0800

    Updated device settings

--- react-playground/home-automation/src/crestron/crestron-device.tsx
+++ react-playground/home-automation/src/crestron/crestron-device.tsx
@@ -2,18 +2,37 @@ import React from 'react';
 import { Device } from './crestron-interfaces';
 import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
+import LightbulbOutlinedIcon from '@mui/icons-material/LightbulbOutlined';
+import PowerSettingsNewOutlinedIcon from '@mui/icons-material/PowerSettingsNewOutlined';
+import DeviceThermostatOutlinedIcon from '@mui/icons-material/DeviceThermostatOutlined';
+import DeviceButtons from './components/device-buttons';
 
 export interface Props {
   device: Device;
 }
 
 export default function CrestronDevice(props: Props): JSX.Element {
+  const DeviceIcon = (props: Props): JSX.Element => {
+    switch (props.device.deviceGroup) {
+      case 'Dimmer':
+        return <LightbulbOutlinedIcon />;
+      case 'Hvac':
+        return <DeviceThermostatOutlinedIcon />;
+      default:
+        return <PowerSettingsNewOutlinedIcon />;
+    }
+  };
+
   return (
     <>
       <Paper className="tiled-paper" elevation={3}>
         <Typography variant="h6">
-          {props.device.name} {props.device.deviceGroup}
+          <DeviceIcon device={props.device} /> {props.device.name}{' '}
+          {props.device.deviceGroup}
         </Typography>
+        <Paper>
+          <DeviceButtons device={props.device} />
+        </Paper>
       </Paper>
     </>
   );
